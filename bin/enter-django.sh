8db1da341dc844ec453bb46f1225843d95b90573
Oscar Garcia  Fuentes <poimsm@gmail.com>
2024-05-17 23:52:16 -0400

    fix: command bin typo

--- bin/enter-django.sh
+++ bin/enter-django.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# case "$(uname -s)" in    
-#     CYWGWIN*|MSYS*|MINGW*)
-#         echo 'Windows'
-#         winpty docker exec -it card_django bash    
-#     ;;
-
-#     *)
-#         echo 'Linux'
-#         docker exec -it card_django bash
-#     ;;
-# esac
-
 source ./bin/commands.sh
 
 CMD_PREFIX=$(detectar_os)
 
-${CMD_PREFIX} docker exec -it flask sh
+${CMD_PREFIX} docker exec -it card_django bash
 
