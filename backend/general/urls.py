8fed53a507d5bd216ec56150c55e6895221b7fbb
Oscar Garcia  Fuentes <poimsm@gmail.com>
2024-06-10 00:52:19 -0400

    feat: add user deletion guide

--- backend/general/urls.py
+++ backend/general/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from .views import policy
+from django.urls import re_path
+from .views import policy, delete
 
 app_name = 'general'
 
 urlpatterns = [
-    path('policy/', policy, name='policy'),
+    re_path(r'^policy\/?$', policy, name='policy'),
+    re_path(r'^how-to-delete-account\/?$', delete, name='delete'),
 ]
--- backend/general/views.py
+++ backend/general/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def policy(request):
     return render(request, 'policy.html')
+
+def delete(request):
+    return render(request, 'delete.html')
